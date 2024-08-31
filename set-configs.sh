61f3d958a720f8c6a3763f8bbe4ab9ebfa275e8b
q3e4ir <durychyaroslav@gmail.com>
2024-08-31 23:14:02 +0300

    commit

--- set-configs.sh
+++ set-configs.sh
@@ -10,9 +10,3 @@ sudo nixos-rebuild switch
 #vscode
 sudo cp -r /home/q3e4ir/dotfiles/vscodium/settings.json /home/q3e4ir/.config/VSCodium/User/
 sudo cp -r /home/q3e4ir/dotfiles/vscodium/.vscode-oss /home/q3e4ir/
-codium --install-extension arrterian.nix-env-selector
-codium --install-extension jnoortheen.nix-ide
-codium --install-extension ms-python.debugpy
-codium --install-extension ms-python.python
-codium --install-extension rogalmic.bash-debug
-codium --install-extension zhuangtongfa.material-theme

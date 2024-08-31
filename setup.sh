93911c4afb5f3a505b5ae6fcc6f39e919f50e945
q3e4ir <durychyaroslav@gmail.com>
2024-08-31 19:21:50 +0300

    commit

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 
 sudo cp -r /home/q3e4ir/git-sync/config-links/configuration.nix /etc/nixos/
 sudo nixos-rebuild switch
+sudo cp -r /home/q3e4ir/git-sync/vscodium/settings.json /home/q3e4ir/.config/VSCodium/User/

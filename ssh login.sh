5e34e89db877859d9c8bca740501d992c20c75cb
q3e4ir <durychyaroslav@gmail.com>
2024-08-31 18:23:15 +0300

    commit

--- ssh login.sh
+++ ssh login.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ssh-keygen -t rsa -b 4096 -P passphrase 
+ssh-keygen -t rsa -b 4096 -P "" -f /home/q3e4ir/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
 
 echo "https://github.com/settings/ssh/new"

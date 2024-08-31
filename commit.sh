953553c33bc040e1516372cb8fd929fba26990a7
q3e4ir <durychyaroslav@gmail.com>
2024-08-31 23:41:10 +0300

    commit

--- commit.sh
+++ commit.sh
@@ -3,8 +3,7 @@
 cd ~/dotfiles
 
 sudo cp -r /home/q3e4ir/.config/VSCodium/User/settings.json /home/q3e4ir/dotfiles/vscodium
-
-#sudo cp -r /home/q3e4ir/.vscode-oss /home/q3e4ir/dotfiles/vscodium
+sudo cp -r /home/q3e4ir/.vscode-oss/extensions/extensions.json /home/q3e4ir/dotfiles/vscodium
 
 git add .
 git commit -m 'commit'
